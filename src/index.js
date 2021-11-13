460988d18233a69235b389e7ad5afd9788698cd9
Jenelle <jschilcher8@gmail.com>
2021-11-13 16:06:19 -0600

    index setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/app";
+
+ReactDOM.render(
+    <Router>
+        <React.StrictMode>
+           <App/>
+        </React.StrictMode>
+    </Router>,
+   document.getElementById("root"));
