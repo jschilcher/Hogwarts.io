c1693b9484ba2dabf39cfc2b159b4ab6fd1eb87b
Jenelle <jschilcher8@gmail.com>
2021-11-14 09:53:37 -0600

    Axios setup on characters

--- src/components/Characters/characters.jsx
+++ src/components/Characters/characters.jsx
@@ -3,6 +3,18 @@ import axios from "axios";
 
 const Characters = () => {
     const [characterData, setCharacterData] = useState([]);
+
+    useEffect(() => {
+        document.title = "Characters - Hogwarts";
+      }, []);
+
+    const fetchAllCharacters = async (event) => {
+        await axios 
+        .get(`http://hp-api.herokuapp.com/api/characters`)
+        .then((response) => {
+            setCharacterData(response.data.results);
+        }).catch(err => consolelog(err))
+    };
 }
 
 export default Characters;
